d951dd5913d6fc313911730c0612cf33ad6ade78
FranRode <98495085+FranRode@users.noreply.github.com>
2022-08-25 11:20:01 -0300

    Update test2.py

--- test2.py
+++ test2.py
@@ -2,7 +2,7 @@ import pytest
 import prog1
 
 def comprobar_ingreso_string():
-	assert type(prog1.p1.retornar_nombre()) == "str"
+	assert isistance(prog1.p1.retornar_nombre(),"str") == True
 def comprobar_ingreso_int():
-	assert type(prog1.p1.retornar_edad()) == "int"
+	assert isistance(prog1.p1.retornar_edad(),"int") == False
 
