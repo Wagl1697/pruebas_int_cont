81d5412154108f19b81c108a3f967266fa6d15cb
FranRode <98495085+FranRode@users.noreply.github.com>
2022-08-29 12:58:25 -0300

    Delete test2.py

--- test2.py
+++
@@ -1,8 +0,0 @@
-import pytest
-import prog1
-
-def comprobar_ingreso_string():
-	assert isistance(prog1.p1.retornar_nombre(),"str") == True
-def comprobar_ingreso_int():
-	assert isistance(prog1.p1.retornar_edad(),"int") == False
-
